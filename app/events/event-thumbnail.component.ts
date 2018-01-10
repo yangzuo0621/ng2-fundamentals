885a24b0d3b8ff23262cbda97c77cfbce9d31dd0
Zuo Yang <zuya@microsoft.com>
2018-01-10 22:52:01 +0800

    Styling Components

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -2,7 +2,11 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
     selector: 'event-thumbnail',
-    templateUrl: 'app/events/event-thumbnail.component.html'
+    templateUrl: 'app/events/event-thumbnail.component.html',
+    styles: [`
+        .pad-left { margin-left: 10px; }
+        .well div { color: #bbb; }
+    `]
 })
 export class EventThumbnailComponent {
     @Input() event: any;
