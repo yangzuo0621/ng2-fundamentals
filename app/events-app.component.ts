4dd3d59f7c6f096d6453c246299ba867b3055238
Zuo Yang <zuya@microsoft.com>
2018-01-10 20:18:24 +0800

    Communicating Between Angular Components

--- app/events-app.component.ts
+++ app/events-app.component.ts
@@ -2,6 +2,6 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'events-app',
-    template: '<h2>Hello World</h2>'
+    template: '<events-list></events-list>'
 })
 export class EventsAppComponent { }
