e025ea25003db674547941eacea504036185cfb5
Zuo Yang <zuya@microsoft.com>
2018-01-11 18:21:18 +0800

    Diving Deeper into Template-based Forms

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import {
     EventsListComponent,
@@ -22,6 +23,8 @@ import { AuthService } from "./user/auth.service";
 @NgModule({
     imports: [
         BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
         RouterModule.forRoot(appRoutes)
     ],
     declarations: [
