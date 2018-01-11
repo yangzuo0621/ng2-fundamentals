67899bc33a028590b0166099ea9f5d65da399083
Zuo Yang <zuya@microsoft.com>
2018-01-11 14:21:02 +0800

    Organizing Your Exports with Barrels

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
 
+import {
+    EventsListComponent,
+    EventThumbnailComponent,
+    EventService,
+    EventDetailsComponent,
+    EventsListResolver,
+    EventRouteActivator,
+    CreateEventComponnet
+} from './events/index';
+
 import { EventsAppComponent } from "./events-app.component";
-import { EventsListComponent } from "./events/events-list.component";
-import { EventThumbnailComponent } from "./events/event-thumbnail.component";
 import { NavBarComponent } from "./nav/navbar.component";
-import { EventService } from "./events/shared/event.service";
 import { ToastrService } from "./common/toastr.service";
-import { EventDetailsComponent } from "./events/event-details/event-details.component";
 import { appRoutes } from "./routes";
-import { CreateEventComponnet } from "./events/create-event.component";
 import { Error404Component } from "./errors/404.component";
-import { EventRouteActivator } from "./events/event-details/event-route-activator.service";
-import { EventsListResolver } from "./events/events-list-resolver.service";
 
 @NgModule({
     imports: [
