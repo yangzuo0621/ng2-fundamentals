ce60fea866de21736acce9244a0bc4cd2d8144fc
Zuo Yang <zuya@microsoft.com>
2018-01-11 09:52:41 +0800

    Creating Reusable Angular Services and Third Party Services

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,6 +5,8 @@ import { EventsAppComponent } from "./events-app.component";
 import { EventsListComponent } from "./events/events-list.component";
 import { EventThumbnailComponent } from "./events/event-thumbnail.component";
 import { NavBarComponent } from "./nav/navbar.component";
+import { EventService } from "./events/shared/event.service";
+import { ToastrService } from "./common/toastr.service";
 
 @NgModule({
     imports: [BrowserModule],
@@ -13,6 +15,10 @@ import { NavBarComponent } from "./nav/navbar.component";
         EventsListComponent,
         EventThumbnailComponent,
         NavBarComponent
+    ],
+    providers: [
+        EventService,
+        ToastrService
     ],
     bootstrap: [EventsAppComponent]
 })
