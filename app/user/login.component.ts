6512c25b719056c7e6105e92978bb3e6fc914e31
Zuo Yang <zuya@microsoft.com>
2018-01-11 15:36:49 +0800

    Creating Template-based Form

---
+++ app/user/login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    templateUrl: 'app/user/login.component.html'
+})
+export class LoginComponent {
+
+    login(formValues) {
+        console.log(formValues)
+    }
+}
