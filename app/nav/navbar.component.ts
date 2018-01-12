e2faf1009860a4082e4560bcfe190cd6ccd1b015
Zuo Yang <zuya@microsoft.com>
2018-01-12 20:00:02 +0800

    Implementing the Session Search

--- app/nav/navbar.component.ts
+++ app/nav/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { AuthService } from "../user/auth.service";
+import { ISession, EventService } from "../events/index";
 
 @Component({
     selector: 'nav-bar',
@@ -11,6 +12,16 @@ import { AuthService } from "../user/auth.service";
         li > a.active { color: #F97924; }
     `]
 })
-export class NavBarComponent { 
-    constructor(private auth: AuthService) { }
+export class NavBarComponent {
+    searchTerm: string = "";
+    foundSessions: ISession[];
+
+    constructor(private auth: AuthService, private eventService: EventService) { }
+
+    searchSessions(searchTerm) {
+        this.eventService.searchSessions(searchTerm).subscribe(sessions => {
+            this.foundSessions = sessions;
+            console.log(this.foundSessions);
+        });
+    }
 }
