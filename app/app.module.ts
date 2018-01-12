90608cd6f1abf850b84615a02381e0037f49ba3e
Zuo Yang <zuya@microsoft.com>
2018-01-12 10:00:24 +0800

    Content Projection

--- app/app.module.ts
+++ app/app.module.ts
@@ -18,6 +18,7 @@ import {
 import { EventsAppComponent } from "./events-app.component";
 import { NavBarComponent } from "./nav/navbar.component";
 import { ToastrService } from "./common/toastr.service";
+import { CollapsibleWellComponent } from "./common/collapsible-well.component";
 import { appRoutes } from "./routes";
 import { Error404Component } from "./errors/404.component";
 import { AuthService } from "./user/auth.service";
@@ -38,7 +39,8 @@ import { AuthService } from "./user/auth.service";
         CreateEventComponnet,
         Error404Component,
         CreateSessionComponent,
-        SessionListComponnet
+        SessionListComponnet,
+        CollapsibleWellComponent
     ],
     providers: [
         EventService,
