f56022969e7ea2ec709cafc2ac4f0851f481e404
Zuo Yang <zuya@microsoft.com>
2018-01-15 09:15:43 +0800

    Routing to the same Component

--- app/common/modal-trigger.directive.ts
+++ app/common/modal-trigger.directive.ts
@@ -1,12 +1,12 @@
-import { Directive, OnInit, Inject, ElementRef,  } from "@angular/core";
+import { Directive, OnInit, Inject, ElementRef, Input } from "@angular/core";
 import { JQ_TOKEN } from "./jQuery.service";
 
 @Directive({
     selector: "[modal-trigger]"
 })
 export class ModalTriggerDirective implements OnInit {
-
     private el: HTMLElement;
+    @Input('modal-trigger') modalId: string;
 
     constructor(ref: ElementRef, @Inject(JQ_TOKEN) private $ : any) {
         this.el = ref.nativeElement;
@@ -14,7 +14,7 @@ export class ModalTriggerDirective implements OnInit {
 
     ngOnInit() {
         this.el.addEventListener('click', e => {
-            this.$('#simple-modal').modal({});
+            this.$(`#${this.modalId}`).modal({});
         });
     }
 }
--- app/events/event-details/event-details.component.ts
+++ app/events/event-details/event-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { EventService } from "../shared/event.service";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Params } from "@angular/router";
 import { IEvent, ISession } from "../shared/index";
 
 @Component({
@@ -21,9 +21,10 @@ export class EventDetailsComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.event = this.eventService.getEvent(
-            +this.route.snapshot.params['id']
-        );
+        this.route.params.forEach((params: Params) => {
+            this.event = this.eventService.getEvent(+params['id']);
+            this.addMode = false;
+        });
     }
 
     addSession() {
